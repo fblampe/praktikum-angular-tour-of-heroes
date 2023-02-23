6cd20e4e19be8b10ecf1c39ca2a382e58bcaac7b
paul <Paul@test.de>
2023-02-23 09:11:42 +0100

    Hero component

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {hero = 'Windstorm';
+}
+export interface Hero {
+  id: number;
+  name: string;
+}
+
+
